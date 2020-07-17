872a4d4ebed1f162f51542fd84f11f4b3e2c85c4
Erick Malta <erickmalta100@gmail.com>
2020-07-17 01:38:29 -0300

    feat: :sparkles: Added tests for the persistence class

--- src/servicies/persistence.spec.ts
+++ src/servicies/persistence.spec.ts
@@ -5,16 +5,41 @@
 // Podemos também, fazer grupos de testes utilizando o describe
 // Estrutura de teste:
 
-describe('TESTANDO ALGUMA COISA', () => {
-  it('should return one', () => {
-    const number = 1;
-    expect(number).toBe(1);
-  });
-});
+import { Persistence } from './persistence';
 
-describe('TESTANDO OUTRA COISA', () => {
-  test('should return Erick', () => {
-    const nome = 'Erick';
-    expect(nome).toBe('Erick');
+describe('Persistence', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should return undefined', () => {
+    // SUT (System under test) por convenção é o nome por convenção
+    //  De classes, objetos, etc que estão sendo criados para ser testados
+    const sut = new Persistence();
+    // Espero que o método não retorne nada
+    // Teste inútil, mas criado
+    expect(sut.saveOrder()).toBe(undefined);
+  });
+  it('should call console.log once', () => {
+    const sut = new Persistence();
+    // Spy on vai ficar espiando o meu objeto e analisar
+    // por chamadas de métodos e etc
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+    // Esse teste, acontecendo só uma vez, funciona como deveria
+    // Mas, se colocarmos mais um teste que executa um log, ele pode vir a falhar
+    // Porque esse consoleSpy vai contar dois logs realizados
+    // Por isso, precisamos falar para o JEST limpar esses
+    // Mocks depois de cada teste
+  });
+
+  it('should call console.log with "Pedido salvo com sucesso"', () => {
+    const sut = new Persistence();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    // Espera que a função seja chamada com exatamente aquele argumento
+    // Uma vírgula diferente e o teste falharia
+    expect(consoleSpy).toHaveBeenCalledWith('Pedido salvo com sucesso');
   });
 });
